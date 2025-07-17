19f49a4270829ff5b6b6de6aba114701bc66ce06
marcosrosa0812 <marcosrosa@memengenharia.com.br>
2025-07-16 21:25:49 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bem-vindo ao Estetix</h1>
+      <p>Protótipo funcional carregado com sucesso!</p>
+    </div>
+  )
+}
+
+export default App
